abf11dc32e0aed7622303d889185f2bbeb0c2891
milianets20 <marian.milianets.ri.2022@lpnu.ua>
2022-11-09 15:51:24 +0200

    dg mast

---
+++ Lab4.05/Lab4.05.cpp
@@ -0,0 +1,40 @@
+﻿// Lab4.05.cpp 
+#include <iostream>
+#include <iomanip>
+#include <time.h>
+using namespace std;
+int main()
+{
+    double x, y;
+    srand((unsigned)time(NULL));
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "x = "; cin >> x;
+        cout << "y = "; cin >> y;
+
+        if ((y >= (x - 2) * (x - 2) - 3) && (y > 0) && (y < x) ||
+            (y >= (x - 2) * (x - 2) - 3) && (y < fabs(x)))
+
+            cout << "yes" << endl;
+        else
+            cout << "no" << endl;
+    }
+    cout << endl << fixed;
+
+
+    for (int i = 0; i < 10; i++)
+    {
+        x = -1. + rand() / RAND_MAX-5;
+        y = -3. + rand() / RAND_MAX-5;
+
+        if ((y >= (x - 2) * (x - 2) - 3) && (y > 0) && (y < x) ||
+            (y >= (x - 2) * (x - 2) - 3) && (y < fabs(x)))
+
+            cout << setw(8) << setprecision(4) << x << " "
+            << setw(8) << setprecision(4) << y << " " << "yes" << endl;
+        else
+            cout << setw(8) << setprecision(4) << x << " "
+            << setw(8) << setprecision(4) << y << " " << "no" << endl;
+    }
+    return 0;
+}
